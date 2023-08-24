3b5e0b5012f4e170351569626fdae824f224ad22
adhithiyan <adhithiyan.work@gmail.com>
2023-08-24 22:16:27 +0530

    add modals to add books

--- script.js
+++ script.js
@@ -16,15 +16,11 @@ function Books(title,author,pages,read){
 
 //function to enter books in the main array myLibrary
 function addBook (){
-    let title = prompt("Enter the title");
-    let author = prompt("Enter the author");
-    let pages = prompt("Enter the pages");
-    let read = prompt("read?");
-
-    obj = new Books(title, author, pages, read);
+    const bookDetails = document.querySelectorAll("input");
+    const obj = new Books(bookDetails[0].value , bookDetails[1].value , bookDetails[2].value);
     myLibrary.push(obj)
+    displayBook()
 }
-
 //function to display books from the main array myLibrary
 function displayBook(){
     for(let i = 0; i <  myLibrary.length; i++){
@@ -32,9 +28,9 @@ function displayBook(){
         //to add a new box on cards grid
         if(count > 3){
             let cards = document.querySelector(".cards");
-            let nextBox = document.createElement("div")
-            nextBox.classList.add(`box-${count}`)
-            cards.appendChild(nextBox)
+            let nextBox = document.createElement("div");
+            nextBox.classList.add(`box-${count}`);
+            cards.appendChild(nextBox);
         }
 
         let box = document.querySelector(`.box-${count}`);
@@ -48,33 +44,45 @@ function displayBook(){
             }
         }
         // //adding the buttons
-        for(let i = 0; i < 3; i++){
+        for(let i = 0; i < 2; i++){
             let btn = document.createElement("button");
             switch(i){
                 case 0:
                     btn.textContent = "Read";
-                    box.append(btn);
-                    break;
-                case 1:
-                    btn.textContent = "Edit";
-                    box.append(btn);
-                    break;
-                case 2:
-                    btn.textContent = "Remove";
                     let div = document.createElement("div");
                     div.appendChild(btn);
-                    div.classList.add("removeBtn");
+                    div.classList.add("readBtnOutside");
                     box.append(div);
+                    break;
+                case 1:
+                    btn.textContent = "Remove";
+                    let div1 = document.createElement("div");
+                    div1.appendChild(btn);
+                    div1.classList.add("removeBtn");
+                    box.append(div1);
                     break;
             }
         }
-        count++
+        count++;
     }
 }
 
-let obj1 = new Books("a" , "b" , "c")
-let obj2 = new Books ("d" , "e" , "f")
-myLibrary.push(obj1)
-displayBook() 
+let obj1 = new Books("a" , "b" , "c");
+myLibrary.push(obj1);
+displayBook();
+ 
+
+//add eventlinsteners for dialog element(pop up screen)
+const addBookBtn = document.querySelector(".addBookBtn");
+const closeBookBtn = document.querySelector(".closeBookBtn");
+const modal = document.querySelector(".modal");
+addBookBtn.addEventListener("click" , () => modal.showModal());
+closeBookBtn.addEventListener("click", () => modal.close());
 
 
+//to prevent the form from submiting and to take values from the text input box to display it
+const submitBookBtn = document.querySelector(".submitBookBtn");
+submitBookBtn.addEventListener("click" , (e)=>{
+    addBook();
+    e.preventDefault();
+})
